ddc31ec9def5a86127dfe87546afe188d4d76d1a
Bertram Ieong <bert9bert@users.noreply.github.com>
2014-06-16 21:22:23 -0500

    added test failure conditions to R wrapper test

--- src/test_grow_predict_01.r
+++ src/test_grow_predict_01.r
@@ -1,8 +1,11 @@
+# set directory to where the shared library is stored
 setwd("~/ParallelDecisionTree/src/")
 
+# load the shared libraries compiled in Fortran
 dyn.load("~/ParallelDecisionTree/src/grow_predict_rwrapper.dll")
 is.loaded("grow_predict_rwrapper")
 
+# define data
 n = 100
 p = 3
 
@@ -115,6 +118,14 @@ ytrain = datamat[,3]
 
 # TODO: make sure that xtrain and xtest are DOUBLE matrices
 
-ret = .Fortran("grow_predict_rwrapper",as.integer(n),as.integer(p),as.matrix(xtrain),as.integer(ytrain),as.matrix(xtrain),ytesthat=integer(n))
-ret
+# fit data to tree, and make sure that predicted values on same data
+# is the same as was inputted
+ret = .Fortran("grow_predict_rwrapper",
+	n=as.integer(n), p=as.integer(p),
+	xtrain=as.matrix(xtrain), ytrain=as.integer(ytrain),
+	xtest=as.matrix(xtrain),ytesthat=integer(n))
+
+if(!all(ret$ytrain==ret$ytesthat)) {
+   stop("Test failed.")
+}
 
