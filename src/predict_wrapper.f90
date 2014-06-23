d288246174c827803aaa8bec6218f0634487b864
Bertram Ieong <bert9bert@users.noreply.github.com>
2014-06-22 23:52:30 -0500

    wrote predict wrapper and basic test

---
+++ src/predict_wrapper.f90
@@ -0,0 +1,42 @@
+subroutine predict_wrapper(tag, tagparent, tagleft, tagright, is_topnode, &
+    depth, majority, has_subnodes, splitvarnum, splitvalue, numnodes, &
+    N, P, Xnew, Ynew_pred)
+
+
+	use utils
+	use tree_utils
+	use classification
+
+	implicit none
+	!--- variable declarations ---
+
+
+	! input variables
+	integer, intent(in) :: numnodes, N, P
+
+	integer, intent(in) :: tag(numnodes), tagparent(numnodes), tagleft(numnodes), tagright(numnodes)
+    logical, intent(in) :: is_topnode(numnodes)
+    integer, intent(in) :: depth(numnodes)
+    integer, intent(in) :: majority(numnodes)
+    logical, intent(in) :: has_subnodes(numnodes)
+    integer, intent(in) :: splitvarnum(numnodes)
+    real(dp), intent(in) :: splitvalue(numnodes)
+
+    real(dp), intent(in) :: Xnew(N,P)
+
+	! output (flattened tree) variables
+	integer, intent(out) :: Ynew_pred(N)
+
+	! private variables
+	type (node) :: tree
+
+
+
+	!--- predict with inputted flattened tree ---
+	tree = flat2tree(tag, tagparent, tagleft, tagright, is_topnode, &
+    		depth, majority, has_subnodes, splitvarnum, splitvalue)
+
+	Ynew_pred = predict(tree, Xnew)
+
+    
+end subroutine
--- src/test_grow_predict_02.r
+++ src/test_grow_predict_02.r
@@ -2,25 +2,30 @@
 setwd("~/ParallelDecisionTree/src/")
 # source("test_grow_predict_02.r")
 
-
+TOP_NODE_NUM = 0
 
 # load the shared libraries compiled in Fortran
 dyn.load("~/ParallelDecisionTree/src/grow_wrapper.dll")
+dyn.load("~/ParallelDecisionTree/src/predict_wrapper.dll")
 is.loaded("grow_wrapper")
-
-retlen = 25
+is.loaded("predict_wrapper")
 
 df = read.csv("../data/easy_2var_data.csv")
 
-
 # define data
-n = 100
-p = 2
+
+min_node_obs = 1
+max_depth = 10
+
+retlen = 2^(max_depth + 1 - TOP_NODE_NUM) - 1
 
 datamat = as.matrix(df)
 
 xtrain = datamat[,c(1,2)]
 ytrain = datamat[,3]
+
+n = nrow(xtrain)
+p = ncol(xtrain)
 
 
 # TODO: make sure that xtrain and xtest are DOUBLE matrices
@@ -30,7 +35,7 @@ ytrain = datamat[,3]
 ret = .Fortran("grow_wrapper",
 	n=as.integer(n), p=as.integer(p),
 	xtrain=as.matrix(xtrain), ytrain=as.integer(ytrain),
-	min_node_obs=as.integer(1), max_depth=as.integer(10), retlen=as.integer(retlen),
+	min_node_obs=as.integer(min_node_obs), max_depth=as.integer(max_depth), retlen=as.integer(retlen),
 	tag_padded=integer(retlen),tagparent_padded=integer(retlen),tagleft_padded=integer(retlen),tagright_padded=integer(retlen),is_topnode_padded=integer(retlen),
 	depth_padded=integer(retlen),majority_padded=integer(retlen),has_subnodes_padded=integer(retlen),splitvarnum_padded=integer(retlen),splitvalue_padded=double(retlen),
 	numnodes=integer(1))
@@ -52,6 +57,24 @@ fittedtree = list(
 	numnodes=ret$numnodes
 	)
 
-#if(!all(ret$ytrain==ret$ytesthat)) {
-#   stop("Test failed.")
-#}
+retpred = .Fortran("predict_wrapper",
+	fittedtree$tag,
+	fittedtree$tagparent,
+	fittedtree$tagleft,
+	fittedtree$tagright,
+	fittedtree$is_topnode,
+    fittedtree$depth,
+    fittedtree$majority,
+    fittedtree$has_subnodes,
+    fittedtree$splitvarnum,
+    fittedtree$splitvalue,
+    fittedtree$numnodes,
+    fittedtree$n,
+    fittedtree$p,
+    fittedtree$xtrain, # put back in training data to make sure the prediction gives back the same
+    ynew_pred=integer(fittedtree$n))
+
+
+if(!all(fittedtree$ytrain==retpred$ynew_pred)) {
+   stop("Test failed.")
+}
