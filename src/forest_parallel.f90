cd4edc0237c7124bc61a58a052d442156f243fb7
Bertram Ieong <bert9bert@users.noreply.github.com>
2014-06-23 23:59:22 -0500

    wrote test for bootstrap, made draws for bootstrap random

--- src/forest_parallel.f90
+++ src/forest_parallel.f90
@@ -5,6 +5,7 @@ module forest_parallel
 !   Author: Bertram Ieong
 !------------------------------------------------------------------------------
 
+use random_utils
 use utils
 use tree_utils
 use sort_utils
@@ -25,7 +26,7 @@ subroutine bootstrap(Y, X, numsamps, Y_boot, X_boot)
     integer, intent(in) :: Y(:)
     real(dp), intent(in) :: X(:,:)
     integer, intent(in) :: numsamps
-    integer, intent(out) :: Y_boot(numsamps)
+    integer, allocatable, intent(out) :: Y_boot(:)
     real(dp), allocatable, intent(out) :: X_boot(:,:)
 
     ! Private Variables
@@ -36,7 +37,7 @@ subroutine bootstrap(Y, X, numsamps, Y_boot, X_boot)
 
     ! Debugging variables
     character(len=50) :: fmt
-    logical, parameter :: verbose = .true.
+    logical, parameter :: verbose = .false.
 
 
     ! --- setup ---
@@ -47,11 +48,12 @@ subroutine bootstrap(Y, X, numsamps, Y_boot, X_boot)
     if(N /= size(Y)) stop "Data dimensions do not match."
 
     ! allocate memory to allocatable arrays
+    allocate(Y_boot(numsamps))
     allocate(X_boot(numsamps,P))
 
 
     ! --- create bootstrapped sample ---
-    call random_seed()  ! TODO: look into making this more random
+    call init_random_seed()
     call random_number(randarr)
 
     rand_obs_num = 1 + int(randarr * ((N - 1) + 1))  ! get array of random integers in [1,N]
@@ -62,10 +64,31 @@ subroutine bootstrap(Y, X, numsamps, Y_boot, X_boot)
     enddo
 
     if(verbose .and. P==1) then
-        fmt = '(i5, f10.5, i5, f10.5)'
+        print *, "   Y         X    Y_boot    X_boot"
+
+        fmt = '(i5, f10.5, i10, f10.5)'
         do i=1,N
-            print fmt, Y(i), X(i,1), Y_boot(i), X_boot(i,1)
+            if(i<=numsamps) then
+                print fmt, Y(i), X(i,1), Y_boot(i), X_boot(i,1)
+            else
+                print fmt, Y(i), X(i,1)
+            endif
         enddo
+    else if(verbose .and. P==2) then
+        print *, "  Y1        X1        X2   Y1_boot   X1_boot   X2_boot"
+
+        fmt = '(i5, f10.5, f10.5, i10, f10.5, f10.5)'
+        do i=1,N
+            if(i<=numsamps) then
+                print fmt, Y(i), X(i,1), X(i,2), Y_boot(i), X_boot(i,1), X_boot(i,2)
+            else
+                print fmt, Y(i), X(i,1), X(i,2)
+            endif
+        enddo
+    endif
+
+    if(verbose .and. (P==1 .or. P==2)) then
+        print *, "Note: Above table only prints up to row N. If numsamps>N, then some rows will not be shown."
     endif
 
 end subroutine
@@ -85,7 +108,7 @@ function grow_forest(Y, X, min_node_obs, max_depth, numsamps, numvars, numboots)
     integer :: N, P
     integer :: treenum
 
-    integer :: Y_boot(numsamps)
+    integer, allocatable :: Y_boot(:)
     real(dp), allocatable :: X_boot(:,:)
     logical, allocatable :: variables_selected(:)
     integer :: variables_selected_nums(numvars)
@@ -117,7 +140,7 @@ function grow_forest(Y, X, min_node_obs, max_depth, numsamps, numvars, numboots)
     do treenum=1,numboots
         ! -- randomly select variables for this tree without replacement --
         ! draw indices of variables without replacement that can be split on
-        call random_seed()  ! TODO: look into making this more random
+        call init_random_seed()  ! TODO: make this random
         call random_number(randarr)
 
         do i=1,numvars
@@ -147,11 +170,51 @@ end function
 function test_bootstrap_01() result(exitflag)
     integer :: exitflag
 
+    integer, parameter :: N=10, P=2
+    integer, parameter :: numsamps = 5
+    integer :: Y(N)
+    integer, allocatable :: Y_boot1(:), Y_boot2(:), Y_boot3(:)
+    real(dp) :: X(N,P)
+    real(dp), allocatable :: X_boot1(:,:), X_boot2(:,:), X_boot3(:,:)
+
+    integer :: i,j
 
     exitflag = -1
 
     print *, " "
     print *, "---------- Running Test Function test_bootstrap_01 -------------------"
+
+    ! --- create bootstrap sample on pre-defined data ---
+    Y = (/0,1,2,3,4,5,6,7,8,9/)
+    X(:,1) = (/10,11,12,13,14,15,16,17,18,19/)
+    X(:,2) = (/20,21,22,23,24,25,26,27,28,29/)
+
+    call bootstrap(Y, X, numsamps, Y_boot1, X_boot1)
+    call bootstrap(Y, X, numsamps, Y_boot2, X_boot2)
+    call bootstrap(Y, X, numsamps, Y_boot3, X_boot3)
+
+
+    ! --- test failure conditions ---
+    ! test that bootstrapped data has correct dimensions
+    if(.not. size(Y_boot1)==numsamps) stop "Test failed."
+    if(.not. size(Y_boot2)==numsamps) stop "Test failed."
+    if(.not. size(Y_boot3)==numsamps) stop "Test failed."
+
+    if(.not. all((/size(X_boot1,1),size(X_boot1,2)/)==(/numsamps,P/))) stop "Test failed."
+    if(.not. all((/size(X_boot2,1),size(X_boot2,2)/)==(/numsamps,P/))) stop "Test failed."
+    if(.not. all((/size(X_boot3,1),size(X_boot3,2)/)==(/numsamps,P/))) stop "Test failed."
+
+    ! check that values have been correctly carried
+    do i=1,numsamps
+        do j=1,P
+            if(.not. all(X_boot1(:,j)==Y_boot1+(j*10_dp))) stop "Test failed."
+            if(.not. all(X_boot2(:,j)==Y_boot2+(j*10_dp))) stop "Test failed."
+            if(.not. all(X_boot3(:,j)==Y_boot3+(j*10_dp))) stop "Test failed."
+        enddo
+    enddo
+
+    ! do rudimentary check for randomness
+    if(all(Y_boot1==Y_boot2) .and. all(Y_boot2==Y_boot3)) stop "Test failed."
 
 
     print *, "Test successful if test executed without error."
