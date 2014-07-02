ec66e9077289ad575601716d5f2179b08d24ae41
Bertram Ieong <bert9bert@users.noreply.github.com>
2014-07-01 23:52:57 -0500

    consolidated S4 class definitions and utilities into one R file

--- R/s4class_utils.r
+++
@@ -1,8 +0,0 @@
-#------------------------------------------------------------------------------
-#   Utilities for S4 classes.
-#   Copyright (C) 2014  Bertram Ieong
-#------------------------------------------------------------------------------
-
-
-setOldClass("data.frame")
-setClassUnion("data.frameORvector", c("data.frame", "vector"))
--- R/s4classes.r
+++ R/s4classes.r
@@ -1,7 +1,10 @@
 #------------------------------------------------------------------------------
-#   Creates an S4 class to represent a forest.
+#   This file defines S4 classes that are used in this package.
 #   Copyright (C) 2014  Bertram Ieong
 #------------------------------------------------------------------------------
+
+setOldClass("data.frame")
+setClassUnion("data.frameORvector", c("data.frame", "vector"))
 
 
 setClass("forest", representation(  n = "integer",
@@ -16,5 +19,15 @@ setClass("forest", representation(  n = "integer",
                                     model = "data.frame",
                                     x = "data.frameORvector",
                                     y = "vector",
+                                    fmla = "formula"
+                                    ))
+
+
+setClass("tree", representation(    n = "integer",
+                                    p = "integer",
+                                    min_node_obs = "integer",
+                                    max_depth = "integer",
+                                    numnodes = "integer",
+                                    flattened.nodes = "data.frame",
                                     fmla = "formula"
                                     ))
