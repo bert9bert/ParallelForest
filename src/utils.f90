9f19ab8f6d00b066debb70de92d4998aadb6b832
Bertram Ieong <bert9bert@users.noreply.github.com>
2014-07-08 22:40:12 -0500

    added additional check to newunit function to make sure that we get a good new unit number

--- src/utils.f90
+++ src/utils.f90
@@ -19,7 +19,10 @@ public  :: dp, intdp
 
 contains
 
-! The below function is adapted from http://fortranwiki.org/fortran/show/newunit
+! The function below is adapted from combining code at 
+! http://fortranwiki.org/fortran/show/newunit and code at
+! http://stackoverflow.com/questions/7876075/getting-free-unit-number-in-fortran
+
 ! This is a simple function to search for an available unit.
 ! LUN_MIN and LUN_MAX define the range of possible LUNs to check.
 ! The UNIT value is returned by the function, and also by the optional
@@ -32,10 +35,12 @@ integer function newunit(unit)
   integer, parameter :: LUN_MIN=10, LUN_MAX=1000
   logical :: opened
   integer :: lun
+  integer :: iostat
 ! begin
   newunit=-1
   do lun=LUN_MIN,LUN_MAX
-    inquire(unit=lun,opened=opened)
+    inquire(unit=lun,opened=opened, iostat=iostat)
+    if (iostat .ne. 0) cycle
     if (.not. opened) then
       newunit=lun
       exit
