cfe70e2b782f87f47615f2bd2eb0dd14d6758059
Bertram Ieong <bert9bert@users.noreply.github.com>
2014-07-12 17:10:52 -0500

    added accessors for forest class

--- R/1_s4classes.r
+++ R/1_s4classes.r
@@ -1,12 +1,15 @@
 #------------------------------------------------------------------------------
-#   This file defines S4 classes that are used in this package.
+#   This file defines S4 classes that are used in this package, as well as
+#   get and set functions.
 #   Copyright (C) 2014  Bertram Ieong
 #------------------------------------------------------------------------------
 
 setOldClass("data.frame")
 setClassUnion("data.frameORvector", c("data.frame", "vector"))
 
+## forest class ##
 
+# define the class
 setClass("forest", representation(  n = "integer",
                                     p = "integer",
                                     min_node_obs = "integer",
@@ -23,6 +26,28 @@ setClass("forest", representation(  n = "integer",
                                     depvar.restore.info = "list"
                                     ))
 
+# define the get operator for slots that should be viewable by the user
+setMethod(
+      f="[",
+      signature="forest",
+      definition=function(x,i){
+            if(i=="n"){return(x@n)}else{}
+            if(i=="p"){return(x@p)}else{}
+            if(i=="min_node_obs"){return(x@min_node_obs)}else{}
+            if(i=="max_depth"){return(x@max_depth)}else{}
+            if(i=="numsamps"){return(x@numsamps)}else{}
+            if(i=="numvars"){return(x@numvars)}else{}
+            if(i=="numboots"){return(x@numboots)}else{}
+            if(i=="numnodes"){return(x@numnodes)}else{}
+            if(i=="model"){return(x@model)}else{}
+            if(i=="x"){return(x@x)}else{}
+            if(i=="y"){return(x@y)}else{}
+            if(i=="fmla"){return(x@fmla)}else{}
+      }
+)
+
+
+## tree class ##
 
 setClass("tree", representation(    n = "integer",
                                     p = "integer",
@@ -32,3 +57,4 @@ setClass("tree", representation(    n = "integer",
                                     flattened.nodes = "data.frame",
                                     fmla = "formula"
                                     ))
+
---
+++ man/accessors.forest.rd
@@ -0,0 +1,14 @@
+\name{accessors.forest}
+\alias{[,forest-method}
+\alias{accessors.forest}
+\title{Accessor method for forest class}
+\description{
+  Methods to access public slots in the forest class.
+}
+\usage{
+\S4method{[}{forest}(x, i)
+}
+\arguments{
+  \item{x}{Object of class inheriting from \code{"\link{forest-class}"}.}
+  \item{i}{String value that is either "n", "p", "min_node_obs", "max_depth", "numsamps", "numvars", "numboots", "numnodes", "model", "x", "y", or "fmla"}
+}
