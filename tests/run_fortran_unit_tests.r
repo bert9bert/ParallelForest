9ccb7ffa47806c4219ae8b2d1166f02609de2732
Bertram Ieong <bert9bert@users.noreply.github.com>
2014-07-14 01:03:49 -0500

    minor code formatting

--- tests/run_fortran_unit_tests.r
+++ tests/run_fortran_unit_tests.r
@@ -7,7 +7,7 @@
 
 ### SETUP ###
 library(ParallelForest)
-
 dyn.load('/home/bert/R/x86_64-pc-linux-gnu-library/3.0/ParallelForest/libs/ParallelForest.so')
-testsret = .Fortran("fortran_unit_tests_wrapper", exitflag=integer(1))
 
+testsret = .Fortran("fortran_unit_tests_wrapper", exitflag=integer(1))
+if(testsret!=0) stop("At least one of the Fortran unit tests failed.")
